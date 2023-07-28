b1d449a42d1c8eb11e4be031a7de6d1ef5c65a52
Marat Kabirov <komar_20111@rambler.ru>
2023-07-28 03:27:57 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://7c34b426-9bbf-4e48-8368-64bb767545d5.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
